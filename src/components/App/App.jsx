49539fbd7e917c85e5a593f58536e353b8862535
Lesia <chumachocl@gmail.com>
2024-05-10 13:45:44 +0300

    add css to profile

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,13 @@
 import "./App.css";
+import clsx from "clsx";
 
-import Profile from "./components/Profile/Profile";
-import FriendList from "./components/FriendList/FriendList";
-import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
+import Profile from "../Profile/Profile";
+import FriendList from "../FriendList/FriendList";
+import TransactionHistory from "../TransactionHistory/TransactionHistory";
 
-import userData from "./userData.json";
-import friends from "./friends.json";
-import transactions from "./transactions.json";
+import userData from "../../userData.json";
+import friends from "../../friends.json";
+import transactions from "../../transactions.json";
 
 function App() {
   return (
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,26 +2,26 @@ import css from "./Profile.module.css";
 
 function Profile({ name, tag, location, image, stats }) {
   return (
-    <div>
-      <div>
-        <img src={image} alt={name} />
-        <p>{name}</p>
-        <p>@{tag}</p>
-        <p>{location}</p>
+    <div className={css.container}>
+      <div className={css.containerInfo}>
+        <img className={css.svg} src={image} alt={name} />
+        <p className={css.name}>{name}</p>
+        <p className={css.st}>@{tag}</p>
+        <p className={css.st}>{location}</p>
       </div>
 
-      <ul>
-        <li>
+      <ul className={css.items}>
+        <li className={css.item}>
           <span>Followers</span>
-          <span>{stats.followers}</span>
+          <span className={css.span}>{stats.followers}</span>
         </li>
-        <li>
+        <li className={css.item}>
           <span>Views</span>
-          <span>{stats.views}</span>
+          <span className={css.span}>{stats.views}</span>
         </li>
-        <li>
+        <li className={css.item}>
           <span>Likes</span>
-          <span>{stats.likes}</span>
+          <span className={css.span}>{stats.likes}</span>
         </li>
       </ul>
     </div>
